5346022e39854845f702d1655dcea6aea3557ffe
seohanbin <scshb2000@naver.com>
2016-01-25 17:16:48 +0000

    completed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,10 @@ class HomeController < ApplicationController
   end
 
   def resulttable
-    
+    if params[:startstring].nil?
+      @outputlistsum = Cplist.all
+      return
+    end
     #빈칸있을시 처리방법###
     # if params[:bookname].nil?
     #   params[:bookname] = ""
@@ -21,41 +24,42 @@ class HomeController < ApplicationController
         uri = URI("#{@sumstring}")
         html_doc = Nokogiri::HTML(Net::HTTP.get(uri))
       
-      
         eachlist = Cplist.new
-        
+      
         #이름
         if html_doc.css("td")[0]
           eachlist.cpname = html_doc.css("td")[0].inner_text
         else
-          eachlist.cpname = "nono"
+          eachlist.cpname = "no name"
         end
         
         #사용기간
-        if html_doc.css("td")[1]
-          eachlist.cpdate = html_doc.css("td")[0].inner_text
+        if html_doc.css("td")[0]
+          eachlist.cpdate = html_doc.css("td")[1].inner_text
         else
-          eachlist.cpdate = "nono"
+          eachlist.cpdate = "no date"
         end
         
         #조건
-        if html_doc.css("td")[2]
-          eachlist.cpcondition = html_doc.css("td")[0].inner_text
+        if html_doc.css("td")[0]
+          eachlist.cpcondition = html_doc.css("td")[2].inner_text
         else
-          eachlist.cpcondition = "nono"
+          eachlist.cpcondition = "no cdn"
         end
         
         #발급수량
-        if html_doc.css("td")[3]
-          eachlist.cpamount = html_doc.css("td")[0].inner_text
+        if html_doc.css("td")[0]
+          eachlist.cpamount = html_doc.css("td")[3].inner_text
         else
-          eachlist.cpamount = "nono"
+          eachlist.cpamount = "no amount"
         end
         
         #링크저장
         eachlist.cplink = @sumstring
       
         eachlist.save
+        
+        sleep 5.5
     end
     
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root :to => "home#numbtype"
  
+ 
  get ":controller(/:action(/:id))"
  post ":controller(/:action(/:id))"
 
